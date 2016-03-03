a3b3929c7a8bd467ef4564f30c6d486f92ffdb48
Master0r0 <reece191@msn.com>
2016-03-03 20:14:36 +0000

    Added the basic plugin.yml and the Plugin main file

---
+++ src/com/github/master0r0/horseplus/HorsePlus.java
@@ -0,0 +1,23 @@
+package com.github.master0r0.horseplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * Licensed under the GNU Public License
+ *
+ * Created by Master0r0 on 03/03/2016.
+ */
+public class HorsePlus extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
